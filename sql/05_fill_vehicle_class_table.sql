92e632cdaa175dae0a8181173ca95292c5d32fab
Elitward.Dunn <elitward@gmail.com>
2014-04-10 13:36:59 -0700

    add SQL script file for vehicle class

---
+++ sql/05_fill_vehicle_class_table.sql
@@ -0,0 +1,15 @@
+INSERT into `super_rent`.`vehicle_class` values('ECONOMY',		'CAR',10.00,50.00,200.00); 
+INSERT into `super_rent`.`vehicle_class` values('COMPACT',		'CAR',11.00,55.00,210.00); 
+INSERT into `super_rent`.`vehicle_class` values('MIDSIZE',		'CAR',12.00,60.00,220.00);
+INSERT into `super_rent`.`vehicle_class` values('STANDARD',		'CAR',13.00,65.00,230.00); 
+INSERT into `super_rent`.`vehicle_class` values('FULLSIZE',		'CAR',14.00,70.00,240.00);
+INSERT into `super_rent`.`vehicle_class` values('PREMIUM',		'CAR',15.00,75.00,250.00); 
+INSERT into `super_rent`.`vehicle_class` values('LUXURY',		'CAR',16.00,80.00,260.00);
+INSERT into `super_rent`.`vehicle_class` values('SUV',			'CAR',18.00,90.00,270.00); 
+INSERT into `super_rent`.`vehicle_class` values('VAN',			'CAR',20.00,100.00,280.00);
+
+INSERT into `super_rent`.`vehicle_class` values('FOOT12',		'Truck',15.00,80.00,220.00); 
+INSERT into `super_rent`.`vehicle_class` values('FOOT15',		'Truck',16.00,85.00,230.00); 
+INSERT into `super_rent`.`vehicle_class` values('FOOT24',		'Truck',17.00,80.00,240.00); 
+INSERT into `super_rent`.`vehicle_class` values('BOXTRUCKS',	'Truck',20.00,100.00,260.00); 
+INSERT into `super_rent`.`vehicle_class` values('CARGOVAN',		'Truck',25.00,120.00,300.00); 
