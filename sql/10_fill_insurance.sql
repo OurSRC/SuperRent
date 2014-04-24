3ddb3066d370c2a5a6b6afbb563a21e549dfb5ae
Hang <miaohahn@gmail.com>
2014-04-24 15:56:20 -0700

    update insurance data

--- sql/10_fill_insurance.sql
+++ sql/10_fill_insurance.sql
@@ -1,4 +1,4 @@
-INSERT INTO `super_rent`.`insurance` VALUES ('DW', 300, 2000, 8000);
-INSERT INTO `super_rent`.`insurance` VALUES ('PAI', 200, 700, 2800);
-INSERT INTO `super_rent`.`insurance` VALUES ('PEC', 200, 700, 2800);
-INSERT INTO `super_rent`.`insurance` VALUES ('RAP', 100, 500, 2000);
+INSERT INTO `super_rent`.`insurance` VALUES ('Damage Waiver', -1, -2, -10);
+INSERT INTO `super_rent`.`insurance` VALUES ('Personal Accident Insurance', 200, 700, 2800);
+INSERT INTO `super_rent`.`insurance` VALUES ('Personal Effects Coverage', 200, 700, 2800);
+INSERT INTO `super_rent`.`insurance` VALUES ('Roadside Assistance', 100, 500, 2000);
