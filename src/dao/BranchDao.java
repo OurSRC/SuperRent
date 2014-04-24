0f66bf81f08a60775dc202ecc53c88eedfcdb77e
jenniferyang <jennifer.xi.yang@gmail.com>
2014-04-24 16:29:19 -0700

    add javadoc

--- src/dao/BranchDao.java
+++ src/dao/BranchDao.java
@@ -13,7 +13,9 @@ import entityParser.IntParser;
 import entityParser.StringParser;
 
 /**
- *
+ * <p>
+ *  This class provides basic access methods, for example, find
+ *  for branch entity.</p>
  * @author Xi Yang
  */
 public class BranchDao extends AbstractDao<Branch> {
@@ -35,19 +37,42 @@ public class BranchDao extends AbstractDao<Branch> {
     protected Branch getInstance() {
         return new Branch();
     }
-
+    /**
+     * This method find a {@link Branch} with given {@code id}
+     * @param id branch id
+     * @return branch with branch id, null if non found
+     * @throws DaoException
+     */
     public Branch findById(int id) throws DaoException {
         return findOne("BranchID=" + SqlBuilder.wrapInt(id));
     }
 
+   /**
+     * This method find a {@link Branch} with given {@code name}
+     * @param name branch name
+     * @return branch with branch name, null if non found
+     * @throws DaoException
+     */
     public Branch findByName(String name) throws DaoException {
         return findOne("BranchName=" + SqlBuilder.wrapStr(name));
     }
 
+    /**
+     * This method find a {@link Branch} with given {@code phone}
+     * @param phone branch phone number
+     * @return branch with branch number, null if non found
+     * @throws DaoException
+     */
     public Branch findByPhone(String phone) throws DaoException {
         return findOne("BranchPhone=" + SqlBuilder.wrapStr(phone));
     }
 
+    /**
+     * This method find a {@link Branch} with given {@code address}
+     * @param address branch address
+     * @return branch with branch address, null if non found
+     * @throws DaoException
+     */
     public Branch findByAddress(String address) throws DaoException {
         return findOne("BranchAddress=" + SqlBuilder.wrapStr(address));
     }
