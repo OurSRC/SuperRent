09a13522eca0090023955c7d7af41354c3db410a
Hang <miaohahn@gmail.com>
2014-04-25 12:05:26 -0700

    modify DB

--- sql/18_fill_return_record.sql
+++ sql/18_fill_return_record.sql
@@ -1,4 +1,4 @@
-INSERT INTO `super_rent`.`return_record` VALUES (2, '2014-3-17 10:50:05', 28000, 20, 120100, 5, 2);
-INSERT INTO `super_rent`.`return_record` VALUES (3, '2014-3-17 07:42:00', 0, 20, 150100, 7, 3);
-INSERT INTO `super_rent`.`return_record` VALUES (4, '2014-3-1 08:59:00', 0, 30, 160100, 7, 4);
-INSERT INTO `super_rent`.`return_record` VALUES (5, '2014-4-23 08:05:00', 0, 30, 170100, 8, 5);
+INSERT INTO `super_rent`.`return_record` VALUES (2, '2014-3-17 10:50:05', 20, 120100, 5, 2);
+INSERT INTO `super_rent`.`return_record` VALUES (3, '2014-3-17 07:42:00', 20, 150100, 7, 3);
+INSERT INTO `super_rent`.`return_record` VALUES (4, '2014-3-1 08:59:00', 30, 160100, 7, 4);
+INSERT INTO `super_rent`.`return_record` VALUES (5, '2014-4-23 08:05:00', 30, 170100, 8, 5);
