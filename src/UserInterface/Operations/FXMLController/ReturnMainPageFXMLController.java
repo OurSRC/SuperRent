108f1ba24080678f1edd9c85e7f283ff271c73c2
Vyas <vyas.vk1989@gmail.com>
2014-04-25 17:59:47 -0700

    Return Started.

--- src/UserInterface/Operations/FXMLController/ReturnMainPageFXMLController.java
+++ src/UserInterface/Operations/FXMLController/ReturnMainPageFXMLController.java
@@ -70,55 +70,54 @@ public class ReturnMainPageFXMLController implements Initializable {
     @FXML
     public void SearchRentButtonAction(ActionEvent event) {
 
-        RentalPane.setVisible(true);
         RentCtrl newRentCtrl = new RentCtrl();
         Rent searchRent = newRentCtrl.getRentByContractNumber(Integer.parseInt(RentalAgreementTF.getText()));
-        
-        System.out.println(searchRent.getContractNo());
-        
-        ReserveCtrl newReserveCtrl = new ReserveCtrl();
-        Reservation rentReservation = newReserveCtrl.getReserve(searchRent.getReservationInfold());
-        
-        System.out.println(rentReservation.getVehicleClass());
-        
-        CustomerCtrl newCustomerCtrl = new CustomerCtrl();
-        Customer rentCustomer = newCustomerCtrl.getCustomerById(rentReservation.getCustomerId());
-        
-        VehicleCtrl newVehicleCtrl = new VehicleCtrl();
-        Vehicle rentedVehicle = newVehicleCtrl.getVehicleByVehicleNo(searchRent.getVehicleNo());
-        
-        ReservationNumberTF.setText(rentReservation.getReservationNo());
-        VehiclePlateTF.setText(rentedVehicle.getPlateNo());
-        RentStartDate.setText(searchRent.getTime().toString());
-        CustomerPhoneTF.setText(rentCustomer.getPhone());
-        VehicleModelTF.setText(rentedVehicle.getMode());
-        
-        if (rentReservation.getEquipmentType().size() != 0) {
-            System.out.println("Inside Additional Equipments");
-            ObservableList<String> items = FXCollections.observableArrayList(rentReservation.getEquipmentType());
-            AdditionalEquipmentList.setItems(items);
-        }
+        if (searchRent != null) {
+            RentalPane.setVisible(true);
+            System.out.println(searchRent.getContractNo());
+            ReserveCtrl newReserveCtrl = new ReserveCtrl();
+            Reservation rentReservation = newReserveCtrl.getReserve(searchRent.getReservationInfold());
+            System.out.println(rentReservation.getVehicleClass());
+            CustomerCtrl newCustomerCtrl = new CustomerCtrl();
+            Customer rentCustomer = newCustomerCtrl.getCustomerById(rentReservation.getCustomerId());
 
-        if (!rentReservation.getInsurance().isEmpty()) {
-            ObservableList<String> items = FXCollections.observableArrayList(rentReservation.getInsurance());
-            InsuranceList.setItems(items);
-        }
-        
-        ReturnNavigator.returnRent  = searchRent;
-        ReturnNavigator.returnReservation = rentReservation;
-        
-        
-        /*FadeTransition fadeTransition = new FadeTransition(Duration.millis(500), RentalPane);
-         FadeTransition fadeTransition1 = new FadeTransition(Duration.millis(500), BottomPane);
-         fadeTransition1.setFromValue(0.0);
-         fadeTransition1.setToValue(1.0);
+            VehicleCtrl newVehicleCtrl = new VehicleCtrl();
+            Vehicle rentedVehicle = newVehicleCtrl.getVehicleByVehicleNo(searchRent.getVehicleNo());
 
-         fadeTransition.setFromValue(0.0);
-         fadeTransition.setToValue(1.0);
-         fadeTransition.play();
-         fadeTransition1.play();*/
-        FadeTransitionMethod(RentalPane);
-        FadeTransitionMethod(BottomPane);
+            ReservationNumberTF.setText(rentReservation.getReservationNo());
+            VehiclePlateTF.setText(rentedVehicle.getPlateNo());
+            RentStartDate.setText(searchRent.getTime().toString());
+            CustomerPhoneTF.setText(rentCustomer.getPhone());
+            VehicleModelTF.setText(rentedVehicle.getMode());
+
+            if (rentReservation.getEquipmentType().size() != 0) {
+                System.out.println("Inside Additional Equipments");
+                ObservableList<String> items = FXCollections.observableArrayList(rentReservation.getEquipmentType());
+                AdditionalEquipmentList.setItems(items);
+            }
+
+            if (!rentReservation.getInsurance().isEmpty()) {
+                ObservableList<String> items = FXCollections.observableArrayList(rentReservation.getInsurance());
+                InsuranceList.setItems(items);
+            }
+
+            ReturnNavigator.returnRent = searchRent;
+            ReturnNavigator.returnReservation = rentReservation;
+
+            /*FadeTransition fadeTransition = new FadeTransition(Duration.millis(500), RentalPane);
+             FadeTransition fadeTransition1 = new FadeTransition(Duration.millis(500), BottomPane);
+             fadeTransition1.setFromValue(0.0);
+             fadeTransition1.setToValue(1.0);
+
+             fadeTransition.setFromValue(0.0);
+             fadeTransition.setToValue(1.0);
+             fadeTransition.play();
+             fadeTransition1.play();*/
+            FadeTransitionMethod(RentalPane);
+            FadeTransitionMethod(BottomPane);
+        } else {
+            System.out.println("Invalid Contract number");
+        }
 
     }
 
