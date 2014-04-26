e91ba77945ab5b345fa55522b92ce7a865acf32a
Elitward.Dunn <elitward@gmail.com>
2014-04-25 23:23:16 -0700

    modify weekly price equipment

--- src/ControlObjects/FinanceCtrl.java
+++ src/ControlObjects/FinanceCtrl.java
@@ -12,48 +12,54 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class FinanceCtrl {
+
     private static int BASE_DAILY_IRATE_DIVISION = 100;
 
-    public int cost(Date t1, Date t2, int weekRate, int dayRate, int hourRate){
+    public int cost(Date t1, Date t2, int weekRate, int dayRate, int hourRate) {
         TimeGroup tg = countTimes(t1, t2);
         int rental = tg.cWeeks * weekRate + tg.cDays * dayRate + tg.cHours * hourRate;
         return rental;
     }
-    
-    public int cost(Date t1, Date t2, int weekRate, int dayRate, int hourRate, int baseRate){
+
+    public int cost(Date t1, Date t2, int weekRate, int dayRate, int hourRate, int baseRate) {
         TimeGroup tg = countTimes(t1, t2);
-        if(weekRate<0)
-            weekRate = -1*weekRate*baseRate/BASE_DAILY_IRATE_DIVISION;
-        if(dayRate<0)
-            dayRate = -1*dayRate*baseRate/BASE_DAILY_IRATE_DIVISION;
-        if(hourRate<0)
-            hourRate = -1*hourRate*baseRate/BASE_DAILY_IRATE_DIVISION;
-        
+        if (weekRate < 0) {
+            weekRate = -1 * weekRate * baseRate / BASE_DAILY_IRATE_DIVISION;
+        }
+        if (dayRate < 0) {
+            dayRate = -1 * dayRate * baseRate / BASE_DAILY_IRATE_DIVISION;
+        }
+        if (hourRate < 0) {
+            hourRate = -1 * hourRate * baseRate / BASE_DAILY_IRATE_DIVISION;
+        }
+
         int rental = tg.cWeeks * weekRate + tg.cDays * dayRate + tg.cHours * hourRate;
         return rental;
     }
-    
-    public int rentalCost(Date t1, Date t2, ReservationInfo reserveInfo){
-        if(reserveInfo!=null)
+
+    public int rentalCost(Date t1, Date t2, ReservationInfo reserveInfo) {
+        if (reserveInfo != null) {
             return cost(t1, t2, reserveInfo.getvWeeklyRate(), reserveInfo.getvDailyRate(), reserveInfo.getvHourlyRate());
-        else
+        } else {
             return 0;
+        }
     }
-    
-    public int equipmentCost(Date t1, Date t2, ReserveEquipment reserveEquipment){
-        if(reserveEquipment!=null)
-            return cost(t1, t2, 0, reserveEquipment.getEDailyRate(), reserveEquipment.getEHourlyRate());
-        else
+
+    public int equipmentCost(Date t1, Date t2, ReserveEquipment reserveEquipment) {
+        if (reserveEquipment != null) {
+            return cost(t1, t2, 7 * reserveEquipment.getEDailyRate(), reserveEquipment.getEDailyRate(), reserveEquipment.getEHourlyRate());
+        } else {
             return 0;
+        }
     }
-    
-    public int insuraceCost(Date t1, Date t2, BuyInsurance insurance, int basePrice){
-        if(insurance!=null){
+
+    public int insuraceCost(Date t1, Date t2, BuyInsurance insurance, int basePrice) {
+        if (insurance != null) {
             int week = insurance.getWeeklyRate();
             int day = insurance.getDailyRate();
-            int hour = insurance.getHourlyRate();            
+            int hour = insurance.getHourlyRate();
             return cost(t1, t2, week, day, hour, basePrice);
-        }else{
+        } else {
             return 0;
         }
     }
@@ -63,30 +69,29 @@ public class FinanceCtrl {
         ReservationInfo reserveInfo = reserve.getReserveInfo();
         int rental = rentalCost(reserve.getReturnTime(), reserve.getPickupTime(), reserveInfo);
         int equip = 0;
-        if( tg.cWeeks==0 ){
-            ArrayList<ReserveEquipment> reserveEquipment = reserve.getReserveEquipment();
-            for(ReserveEquipment anEquip: reserveEquipment){
-                equip += equipmentCost(reserve.getReturnTime(), reserve.getPickupTime(), anEquip);
-            }
+        ArrayList<ReserveEquipment> reserveEquipment = reserve.getReserveEquipment();
+        for (ReserveEquipment anEquip : reserveEquipment) {
+            equip += equipmentCost(reserve.getReturnTime(), reserve.getPickupTime(), anEquip);
         }
         int insurance = 0;
         ArrayList<BuyInsurance> reserveInsurances = reserve.getReserveInsurance();
-        for(BuyInsurance anInsu: reserveInsurances){
+        for (BuyInsurance anInsu : reserveInsurances) {
             insurance += insuraceCost(reserve.getReturnTime(), reserve.getPickupTime(), anInsu, reserveInfo.getvDailyRate());
         }
-        
+
         return rental + equip + insurance;
     }
 
     public ArrayList<PaymentItem> calulateReturnCost(Return returnInfo, boolean usePoint, Integer sumOutput) {
         ArrayList<PaymentItem> list = null;
-        if(returnInfo==null)
+        if (returnInfo == null) {
             return null;
-        
+        }
+
         RentCtrl rentCtrl = new RentCtrl();
         ReserveCtrl reserveCtrl = new ReserveCtrl();
         BranchCtrl branchCtrl = new BranchCtrl();
-        
+
         Date returnTime = returnInfo.getReturnTime();
         Rent rent = rentCtrl.getRentByContractNumber(returnInfo.getContractNo());
         Date rentTime = rent.getTime();
@@ -94,43 +99,58 @@ public class FinanceCtrl {
         TimeGroup tg = countTimes(rentTime, returnTime);
         Branch branch = branchCtrl.getBranchById(reserve.getBranchId());
 
-        int cRental =  rentalCost(rentTime, returnTime, reserve.getReserveInfo());
+        int cRental = rentalCost(rentTime, returnTime, reserve.getReserveInfo());
         int cEquip = 0;
-        if( tg.cWeeks==0 ){
-            ArrayList<ReserveEquipment> reserveEquipment = reserve.getReserveEquipment();
-            for(ReserveEquipment anEquip: reserveEquipment){
-                cEquip += equipmentCost(rentTime, returnTime, anEquip);
-            }
+        ArrayList<ReserveEquipment> reserveEquipment = reserve.getReserveEquipment();
+        for (ReserveEquipment anEquip : reserveEquipment) {
+            cEquip += equipmentCost(rentTime, returnTime, anEquip);
         }
         int cInsurance = 0;
         ArrayList<BuyInsurance> reserveInsurances = reserve.getReserveInsurance();
-        for(BuyInsurance anInsu: reserveInsurances){
-            cInsurance += insuraceCost( reserve.getReturnTime(), reserve.getPickupTime(), anInsu, reserve.getReserveInfo().getvDailyRate() );
+        for (BuyInsurance anInsu : reserveInsurances) {
+            cInsurance += insuraceCost(reserve.getReturnTime(), reserve.getPickupTime(), anInsu, reserve.getReserveInfo().getvDailyRate());
         }
-        
-        int cMile = returnInfo.getOdometer()*branch.getPricePerKM();
-        int cFuel = returnInfo.getFuelLevel()*branch.getFuelPrice();
+
+        int cMile = returnInfo.getOdometer() * branch.getPricePerKM();
+        int cFuel = returnInfo.getFuelLevel() * branch.getFuelPrice();
         int cDamage = returnInfo.getDamageCost();
         int cDiscount = 0;
+        if( usePoint ){
+            
+        }
+
         list = new ArrayList<>();
-        
-        PaymentItem pRental       = new PaymentItem(0, PaymentItem.ITEMTYPE.VEHICLE, "Rental", cRental, 1);
-        PaymentItem pEquip        = new PaymentItem(0, PaymentItem.ITEMTYPE.EQUIPMENT, "Equipment", cEquip, 1);
+        PaymentItem pRental = new PaymentItem(0, PaymentItem.ITEMTYPE.VEHICLE, "Rental", cRental, 1);
+        PaymentItem pEquip = new PaymentItem(0, PaymentItem.ITEMTYPE.EQUIPMENT, "Equipment", cEquip, 1);
         PaymentItem pInsurance = new PaymentItem(0, PaymentItem.ITEMTYPE.INSURANCE, "Insurance", cInsurance, 1);
-        PaymentItem pMile           = new PaymentItem(0, PaymentItem.ITEMTYPE.MILE, "Additional Miles", cMile, 1);
-        PaymentItem pFuel           = new PaymentItem(0, PaymentItem.ITEMTYPE.FUEL, "Fuel", cFuel, 1);
-        PaymentItem pDamage     = new PaymentItem(0, PaymentItem.ITEMTYPE.DAMAGE, "Damage", cDamage, 1);
-        PaymentItem pDiscount    = new PaymentItem(0, PaymentItem.ITEMTYPE.POINTEXCHANGE, "Discount", cDiscount, 1);
-        
-        if(cRental!=0)        list.add(pRental);
-        if(cEquip!=0)         list.add(pEquip);
-        if(cInsurance!=0)  list.add(pInsurance);
-        if(cMile!=0)            list.add(pMile);
-        if(cFuel!=0)           list.add(pFuel);
-        if(cDamage!=0)     list.add(pDamage);
-        if(cDiscount!=0)    list.add(pDiscount);
-        
-        sumOutput = cRental+cEquip+cInsurance+cMile+cFuel+cDamage+cDiscount;
+        PaymentItem pMile = new PaymentItem(0, PaymentItem.ITEMTYPE.MILE, "Additional Miles", cMile, 1);
+        PaymentItem pFuel = new PaymentItem(0, PaymentItem.ITEMTYPE.FUEL, "Fuel", cFuel, 1);
+        PaymentItem pDamage = new PaymentItem(0, PaymentItem.ITEMTYPE.DAMAGE, "Damage", cDamage, 1);
+        PaymentItem pDiscount = new PaymentItem(0, PaymentItem.ITEMTYPE.POINTEXCHANGE, "Discount", cDiscount, 1);
+
+        if (cRental != 0) {
+            list.add(pRental);
+        }
+        if (cEquip != 0) {
+            list.add(pEquip);
+        }
+        if (cInsurance != 0) {
+            list.add(pInsurance);
+        }
+        if (cMile != 0) {
+            list.add(pMile);
+        }
+        if (cFuel != 0) {
+            list.add(pFuel);
+        }
+        if (cDamage != 0) {
+            list.add(pDamage);
+        }
+        if (cDiscount != 0) {
+            list.add(pDiscount);
+        }
+
+        sumOutput = cRental + cEquip + cInsurance + cMile + cFuel + cDamage + cDiscount;
         return list;
     }
 
@@ -139,35 +159,40 @@ public class FinanceCtrl {
         BranchCtrl branchCtrl = new BranchCtrl();
         Branch branch = branchCtrl.getBranchById(branchId);
         list.add(new PaymentItem(0, PaymentItem.ITEMTYPE.MEMBERSHIP, "Membership", branch.getClubMemberFeeRate(), years));
+        sumOutput = branch.getClubMemberFeeRate() * years;
         return list;
     }
 
     public Payment createPayment() {
         return null;
     }
-    
-    private class TimeGroup{
+
+    private class TimeGroup {
+
         int cWeeks;
         int cDays;
         int cHours;
     }
-    private TimeGroup countTimes(Date t1, Date t2){
-        long week = 7*24*60*60*1000;
-        long day = 24*60*60*1000;
-        long hour = 60*60*1000;
+
+    private TimeGroup countTimes(Date t1, Date t2) {
+        long week = 7 * 24 * 60 * 60 * 1000;
+        long day = 24 * 60 * 60 * 1000;
+        long hour = 60 * 60 * 1000;
 
         TimeGroup time = new TimeGroup();
         long delta = t1.getTime() - t2.getTime();
-        if(delta<0)
-            delta *=-1;
+        if (delta < 0) {
+            delta *= -1;
+        }
         time.cWeeks = (int) (delta / week);
         delta = delta % week;
         time.cDays = (int) (delta / day);
         delta = delta % day;
-        time.cHours = (int)( delta / hour);
+        time.cHours = (int) (delta / hour);
         delta = delta % hour;
-        if( delta >0)
+        if (delta > 0) {
             time.cHours++;
+        }
         return time;
     }
 }
