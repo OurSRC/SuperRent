64178e7a5b42ddfa625f2cad41cee82504ce03dd
Hang <miaohahn@gmail.com>
2014-04-27 02:17:50 -0700

    add test case

---
+++ test/ControlObjects/CustomerCtrlTest.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ControlObjects;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cesium
+ */
+public class CustomerCtrlTest {
+    
+    public CustomerCtrlTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testCreateCustomer() {
+    }
+
+    @Test
+    public void testUpdateCustomer() {
+    }
+
+    @Test
+    public void testDeleteCustomer() {
+    }
+
+    @Test
+    public void testGetCustomerByPhone() {
+    }
+
+    @Test
+    public void testGetCustomerByUsername() {
+    }
+
+    @Test
+    public void testGetCustomerById() {
+    }
+
+    @Test
+    public void testSearchCustomer() {
+    }
+
+    @Test
+    public void testCheckMembershipActive() {
+    }
+
+    @Test
+    public void testExtendMembership() {
+    }
+
+    @Test
+    public void testPayClubMemberFee() {
+    }
+    
+}
