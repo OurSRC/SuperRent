aaf2644a9f2127ee28739df3ccc540399e70e3cc
Hang <miaohahn@gmail.com>
2014-04-28 19:36:23 -0700

    update DB

---
+++ sql/input_forDemo.bat
@@ -0,0 +1,22 @@
+mysql --user superrent --password=superrent < 0_0_drop_db_tables.sql
+mysql --user superrent --password=superrent < 0_1_create_db_tables.sql
+mysql --user superrent --password=superrent < 1_1_fill_branch_table.sql
+mysql --user superrent --password=superrent < 1_1_fill_vehicle_class_table.sql
+mysql --user superrent --password=superrent < 1_2_fill_equipment_type_table.sql
+mysql --user superrent --password=superrent < 1_3_fill_support_table.sql
+mysql --user superrent --password=superrent < 1_4_fill_insurance_table.sql
+mysql --user superrent --password=superrent < 2_1_fill_vehicle_table.sql
+mysql --user superrent --password=superrent < 2_2_fill_equipment_table.sql
+mysql --user superrent --password=superrent < 2_3_fill_user_table.sql
+mysql --user superrent --password=superrent < 2_4_fill_staff_table.sql
+mysql --user superrent --password=superrent < 3_1_fill_user_table.sql
+mysql --user superrent --password=superrent < 3_2_fill_staff_table.sql
+mysql --user superrent --password=superrent < 3_3_fill_customer_table.sql
+
+
+
+
+
+
+echo finish
+pause
